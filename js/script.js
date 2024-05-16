d7031c5853e7de8d02247b2a7cfe6d4a0316b817
GencoMattia <gencomattia@hotmail.it>
2024-05-16 16:31:21 +0200

    added computer number and gerRandomNumber function

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ if(userWord.toUpperCase() === getReversedWord(userWord).toUpperCase()){
 // }
 // while(userChoice !== "pari" && userChoice !== "dispari");
 
-
+// Scelta giocatore pari o dispari
 let userChoice = prompt("Scegli: pari o dispari?");
 
 while(userChoice !== "pari" && userChoice !== "dispari"){
@@ -36,8 +36,22 @@ while(userChoice !== "pari" && userChoice !== "dispari"){
 
 console.log(userChoice);
 
+// Scelta numero giocatore
 let userNumber = prompt("Scegli un numero compreso tra 1 e 5");
 
 while(userNumber < 1 || userNumber > 5 || Number.isNaN(userNumber)){
     userNumber = prompt("Scegli un numero compreso tra 1 e 5");
 }
+
+console.log(`Numero utente: ${userNumber}`)
+
+// Scelta numero computer
+let computerNumber = getRandomNumber(1, 5);
+
+function getRandomNumber(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+console.log(`Numero computer: ${computerNumber}`)
+
+// Somma numeri e scelta vincitore
