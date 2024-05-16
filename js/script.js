74c2d2cb91a4ed13c676a4741df885a914d5729c
GencoMattia <gencomattia@hotmail.it>
2024-05-16 17:40:29 +0200

    Palindromo: merged if with the function to have the function check if the word is a pali

--- js/script.js
+++ js/script.js
@@ -2,19 +2,21 @@
 
 const userWord = prompt("Scegli una parola");
 
-function getReversedWord(word){
+function getPaliWord(word){
     let reversedWord = "";
     for (i = word.length - 1; i >= 0; i--){
         reversedWord += word[i];
     }
-    return reversedWord;
+    
+    if(word.toUpperCase() === reversedWord.toUpperCase()){
+        return `WOW! ${word.toUpperCase()} e ${reversedWord.toUpperCase()} sono uguali! è un palindtromo`;
+    } else {
+        return `${word.toUpperCase()} e ${reversedWord.toUpperCase()} sono differenti. Perciò niente palindromi oggi`;
+    }
 }
 
-if(userWord.toUpperCase() === getReversedWord(userWord).toUpperCase()){
-    console.log(`WOW! ${userWord} e ${getReversedWord(userWord)} sono uguali! è un palindtromo`);
-} else {
-    console.log(`${userWord} e ${getReversedWord(userWord)} sono differenti. Perciò niente palindromi oggi`);
-}
+console.log(getPaliWord(userWord))
+
 
 
 
