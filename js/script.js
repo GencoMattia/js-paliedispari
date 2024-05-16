c37f6d267d9e13515d16c9ac5c43fb4747953e28
GencoMattia <gencomattia@hotmail.it>
2024-05-16 16:10:57 +0200

    Pari e Dispari: added do-while to ask user pari o dispari

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
-const userWord = prompt.("Scegli una parola");
+// PALINDTROMO
+
+const userWord = prompt("Scegli una parola");
 
 function getReversedWord(word){
     let reversedWord = "";
     for (i = word.length - 1; i >= 0; i--){
         reversedWord += word[i];
     }
-    return reversedWord
+    return reversedWord;
 }
 
-if(userWord.toUpperCase === getReversedWord(userWord).toUpperCase){
-    console.log(`WOW! ${userWord} e ${getReversedWord(userWord)} sono uguali! è un palindtromo`)
+if(userWord.toUpperCase() === getReversedWord(userWord).toUpperCase()){
+    console.log(`WOW! ${userWord} e ${getReversedWord(userWord)} sono uguali! è un palindtromo`);
 } else {
-    console.log(`${userWord} e ${getReversedWord(userWord)} sono differenti. Perciò niente palindromi oggi`)
+    console.log(`${userWord} e ${getReversedWord(userWord)} sono differenti. Perciò niente palindromi oggi`);
 }
+
+
+
+// PARI E DISPARI
+
+let userChoice;
+
+do {
+    userChoice = prompt("Scegli: pari o dispari?");
+}
+while(userChoice !== "pari" && userChoice !== "dispari");
+
+console.log(userChoice)
