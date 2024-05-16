78dde0f36df45efed68899f7547f3f8406dd8e8f
GencoMattia <gencomattia@hotmail.it>
2024-05-16 15:41:22 +0200

    Palindromo: created getReversedWord function

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const userWord = prompt("Scegli una parola");
+
+function getReversedWord(word){
+    let reversedWord = "";
+    for (i = word.length; i >= 0; i--){
+        reversedWord = word[i];
+    }
+    return reversedWord
+}
