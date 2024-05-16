20e1eacca3c333a488ca98a93e0998cc56a6eae7
GencoMattia <gencomattia@hotmail.it>
2024-05-16 15:44:22 +0200

    Correcret getReversedWordFunction

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ const userWord = prompt("Scegli una parola");
 
 function getReversedWord(word){
     let reversedWord = "";
-    for (i = word.length; i >= 0; i--){
-        reversedWord = word[i];
+    for (i = word.length - 1; i >= 0; i--){
+        reversedWord += word[i];
     }
     return reversedWord
 }
+
+console.log(getReversedWord(userWord))
